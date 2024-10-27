dde5a30d89722a74f2d4ad04297f406b98b75f9a
Voldermarus <k.vladik.p@gmail.com>
2024-10-27 15:35:13 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,33 @@
+first = 123
+second = 456
+third = 789
+
+zadanie1 = input('')
+
+if first == second == third:
+    print(3)
+
+# elif first == second or third:     Если строку включить, на выводе будет 2! Почему?
+#     print(2)                       При отключенном втором задании ничего не меняется.
+
+else:
+    print(0)
+
+
+
+first = 42
+second = 69
+third = 42
+
+zadanie2 = input('')
+
+if first == second or third:
+    print(2)
+
+else:
+    print(0)
+
+
+
+
+
